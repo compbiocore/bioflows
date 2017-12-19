2f8f1aaf39c9480971e140fb7c2c02c1785c08e8
ashokrags <ashok.ragavendran@gmail.com>
2017-12-19 17:50:53 -0500

    Added test for SRA run of dual RNA seq

--- test_rnaseq_workflow/test_rnaSeqFlow.py
+++ test_rnaseq_workflow/test_rnaSeqFlow.py
@@ -209,8 +209,7 @@ class TestRnaSeqFlowLocalSlurmPE(TestCase):
 
 class TestRnaSeqFlowSRALocalSlurmSE(TestCase):
     def setUp(self):
-        #self.parmsfile = "test_run_sra_localhost_slurm_se_mus.yaml"
-        self.parmsfile = "test_run_sra_localhost_slurm_se_mus_spnuemonia.yaml"
+        self.parmsfile = "test_run_sra_localhost_slurm_se_mus.yaml"
         self.rw1 = rsw(self.parmsfile)
 
     def test_parse_config(self):
@@ -275,6 +274,38 @@ class TestRnaSeqFlowSRALocalSlurmPE(TestCase):
 
 
 
+class TestRnaSeqFlowSRALocalSlurmSEDual(TestCase):
+    def setUp(self):
+        self.parmsfile = "test_run_sra_localhost_slurm_se_mus_spnuemonia.yaml"
+        self.rw1 = rsw(self.parmsfile)
+
+    def test_parse_config(self):
+        self.rw1.parse_config(self.parmsfile)
+
+        print "\n***** Printing config Parsing ******\n"
+        for k, v in self.rw1.__dict__.iteritems():
+            print k, v
+
+    # def test_symlink_fastqs(self):
+    #     #self.rw1.sample_fastq = {'sampN2': ['/gpfs/scratch/aragaven/test_workflow/N1-BC1_AACCAG_R1.fastq.gz'],
+    #      #                        'sampN3': ['/gpfs/scratch/aragaven/test_workflow/N3-BC3_AGTGAG_R1.fastq.gz']}
+    #     self.rw1.parse_sample_info_from_file()
+    #     self.rw1.symlink_fastqs()
+
+    def test_run_chain_commands(self):
+        self.rw1.parse_prog_info()
+        self.rw1.test_paths()
+        self.rw1.download_sra_cmds()
+        # self.rw1.symlink_fastqs()
+        # self.rw1.set_base_kwargs()
+        self.rw1.chain_commands()
+        luigi.build([TaskFlow(tasks=self.rw1.allTasks, task_name=self.rw1.bioproject)], local_scheduler=True,
+                    workers=len(self.rw1.sample_fastq_work.keys()), lock_size=1)
+
+        ## Need to write another test to test the luigi web interface
+
+        # luigi.build([TaskFlow(tasks=self.rw1.allTasks)], local_scheduler=False, workers=2, lock_size=3)
+
 
 
 if __name__ == '__main__':
@@ -305,9 +336,9 @@ if __name__ == '__main__':
     # runner.run(suite)
 
     ## Runs  only a specific class for PE C elegans from my mac to CCV
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestRnaSeqFlowLocalToRemotePE)
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    # suite = unittest.TestLoader().loadTestsFromTestCase(TestRnaSeqFlowLocalToRemotePE)
+    # runner = unittest.TextTestRunner()
+    # runner.run(suite)
 
     ## Runs  only a specific class for SE Mouse locally on CCV
 
@@ -327,8 +358,14 @@ if __name__ == '__main__':
     # runner = unittest.TextTestRunner()
     # runner.run(suite)
 
-    ## Runs  only a specific class for SE mus from SRA locally on CCV
+    ## Runs  only a specific class for PE mus from SRA locally on CCV
     #
     # suite = unittest.TestLoader().loadTestsFromTestCase(TestRnaSeqFlowSRALocalSlurmPE)
     # runner = unittest.TextTestRunner()
     # runner.run(suite)
+
+    # Runs  only a specific class for SE  dual RNA Seq mus and S. pnuemonia from SRA locally on CCV
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRnaSeqFlowSRALocalSlurmSEDual)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
