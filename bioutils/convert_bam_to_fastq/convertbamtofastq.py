7de4df5608854e3be4fbb4745b02ec1ede448181
ashokrags <ashok.ragavendran@gmail.com>
2018-01-09 13:34:52 -0500

    Added commands logger for bamtoFastq function

--- bioutils/convert_bam_to_fastq/convertbamtofastq.py
+++ bioutils/convert_bam_to_fastq/convertbamtofastq.py
@@ -1,4 +1,4 @@
-import subprocess as sp, sys
+import subprocess as sp, sys,
 
 class bamtofastq:
     sample_list=dict()
@@ -25,6 +25,10 @@ class bamtofastq:
         return
 
     def picard_converter(self):
+
+        f = open("bam_fastq_picard_commands.log", 'w')
+        f.write("*** Convert Bam to Fastq Using Picard ***\n")
+
         for samp,filepath in self.sample_list.iteritems():
             cmd = " sbatch -t 05:00:00 --mem=10g -n 4 --wrap='source activate cbc_conda;"
             cmd = cmd + " picard -Xmx 10000m SamToFastq I=" + file  +  " "
@@ -35,10 +39,14 @@ class bamtofastq:
             cmd = cmd + " gzip " + self.output_folder + "/pic_" + samp + "_r2.fq ;"
             cmd = cmd + " gzip " + self.output_folder + "/pic_" + samp + "_up.fq'"
             sp.getoutput(cmd, shell=True)
+            f.write(cmd + '\n')
+        f.close()
 
         return
 
     def biobambam_converter(self):
+        f = open("bam_fastq_biobambam_commands.log", 'w')
+        f.write("*** Convert Bam to Fastq Using BioBamBam ***\n")
         for samp,filepath in self.sample_list.iteritems():
             cmd = " sbatch -t 05:00:00 --mem=10g -n 4 --wrap='source activate cbc_conda; bamtofastq"
             cmd = cmd + " F="  + self.output_folder + "/bb_" + samp + "_r1.fq.gz "
@@ -47,6 +55,8 @@ class bamtofastq:
             cmd = cmd + " O2=" + self.output_folder + "/bb_" + samp + "_um_r2.fq.gz "
             cmd = cmd + " filename=" + filepath + " gz=1'"
             sp.getoutput(cmd, shell=True)
+            f.write(cmd + '\n')
+        f.close()
         return
 
 if __name__ == '__main__':
